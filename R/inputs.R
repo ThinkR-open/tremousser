a204705e9a64998c91c645de678f9e13111fb557
Arthur Bréant <arthur@thinkr.fr>
2024-02-19 17:46:32 +0100

    doc: update function documentation

--- R/inputs.R
+++ R/inputs.R
@@ -16,8 +16,17 @@ input <- function(name) {
   }
 }
 
+#' Button component
+#'
+#' @param ... Arguments to be passed to the underlying Button component.
+#'
 #' @export
 Button <- component("Button")
 
+#' Button input
+#'
+#' @param inputId The input slot that will be used to access the value.
+#' @param ... Arguments to be passed to the underlying actionButton function.
+#'
 #' @export
 actionButton <- input("Button")
